f0ed0b4c2cf05906e2116a1d48c16125962e3a75
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-09-01 19:42:46 -0700

    Initial Commit

---
+++ cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by airbr on 9/1/18.
+//  Copyright © 2018 airbr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
