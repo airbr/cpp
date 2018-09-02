fa17d0b5a1d130d66616793f33052ff699308e96
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-09-02 10:32:35 -0700

    Basics

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # C++ Exercises
 
 ### And also learning to use XCode.
+
+#### Current: [Exercise](https://www.learncpp.com/cpp-tutorial/12-comments/)
+
+
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,10 +6,21 @@
 //  Licensed Creative Commons
 //  Attribution 4.0 International (CC BY 4.0)
 
+
 #include <iostream>
 
 int main()
 {
+    // Declaration
+    int x;
+    x = 5;
+    std::cout << (x + 5);
+
+    
     std::cout << "Hello world!" << std::endl;
+    /*
+     *  Pretty comments 
+     *
+     */
     return 0;
 }
