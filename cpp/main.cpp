bb5ed94aff40bf0612744da201a894ec1605c104
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-09-02 16:39:59 -0700

    Early days

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,20 +7,42 @@
 //  Attribution 4.0 International (CC BY 4.0)
 
 
+
+//int main()
+//{
+//    // Declaration
+//    int x;
+//    x = 5;
+//    std::cout << (x + 5);
+//
+//
+//    std::cout << "Hello world!" << std::endl;
+//    /*
+//     *  Pretty comments
+//     *
+//     */
+//    return 0;
+//}
+
+// Example of an unitialized variable problem:
+
 #include <iostream>
+
+void doNothing(const int &x)
+{
+}
 
 int main()
 {
-    // Declaration
-    int x;
-    x = 5;
-    std::cout << (x + 5);
-
+    // define an integer variable named x
+    int x; // this variable is uninitialized
     
-    std::cout << "Hello world!" << std::endl;
-    /*
-     *  Pretty comments 
-     *
-     */
+    doNothing(x); // make compiler think we're using this variable
+    
+    // print the value of x to the screen (who knows what we'll get, because x is uninitialized)
+    std::cout << x;
+    
     return 0;
 }
+
+// Rule: Make sure all of your variables have known values (either through initialization or assignment)
